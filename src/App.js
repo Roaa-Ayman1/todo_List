5e0c7798e7642ef0125ca591f22c0201b3265403
Roaa Ayman <roaa31784@gmail.com>
2024-02-29 16:55:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [x,setx] = useState([]);
+  const TaskRef = useRef();
+
+  const AddTask = () => {
+    const value = TaskRef.current.value;
+    const task = {completed : false , value};
+    setx([...x,task]);
+    TaskRef.current.value = "";
+  }
+
+  const Undo = (index) => {
+    const newx = [...x];
+    newx[index].completed = !newx[index].completed;
+    setx(newx)
+  }
+
+  const DeleteTask = (index) => {
+    const newx = [...x];
+    newx.splice(index,1)
+    setx(newx)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>TODO LIST</h3>
+
+      <ul>
+        {
+          x.map((item , index) => {
+            return <div className='taskDiv'>
+              <li className={item.completed? "completedStyle" : ""} onClick={ ()=>Undo(index) }>{item.value}</li>
+              <span onClick={ ()=>DeleteTask(index) }>❌</span>
+            </div>
+          })
+        }
+      </ul>
+
+      <input ref={TaskRef} placeholder='Enter new task'></input>
+      <button onClick={AddTask}>Add</button>
     </div>
   );
 }
